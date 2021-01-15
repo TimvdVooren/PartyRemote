a4cd94551d5ebdadcc56a00c1df7b03abfcdafb3
TimvdVooren <timvandervooren@gmail.com>
2021-01-15 12:06:09 +0100

    [Fixed] connection visualisation

--- app/src/main/java/com/example/partyremote/views/MainActivity.kt
+++ app/src/main/java/com/example/partyremote/views/MainActivity.kt
@@ -35,7 +35,37 @@ class MainActivity : AppCompatActivity() {
             viewModel.initiateBluetoothProcess()
         }
 
-//        viewModel.btHandler = BluetoothHandler(BluetoothAdapter.getDefaultAdapter())
+        viewModel.isBtConnected.observe(this, Observer { isConnected ->
+            if (isConnected) {
+                binding.mainBluetoothIcon.setImageResource(R.drawable.ic_bluetooth_connected_24)
+                binding.mainBluetoothStatusImage.imageTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(applicationContext, R.color.connected_green)
+                )
+                binding.mainTitle.text = getString(R.string.main_title_connected)
+                binding.mainBluetoothStatusText.text = getString(R.string.main_bt_status_connected)
+                binding.mainControlButton.visibility = View.VISIBLE;
+                binding.mainControlButton.isEnabled = true;
+
+                binding.mainBluetoothButton.setOnClickListener {}
+            } else {
+                binding.mainBluetoothIcon.setImageResource(R.drawable.ic_bluetooth_disabled_24)
+                binding.mainBluetoothStatusImage.imageTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(applicationContext, R.color.searching_blue)
+                )
+                binding.mainTitle.text = getString(R.string.main_title_disconnected)
+                binding.mainBluetoothStatusText.text = getString(R.string.main_bt_status_disconnected)
+                binding.mainControlButton.visibility = View.INVISIBLE;
+                binding.mainControlButton.isEnabled = false;
+
+                binding.mainBluetoothButton.setOnClickListener {
+                    viewModel.initiateBluetoothProcess()
+                }
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
 
         //if bluetooth is not enabled then create Intent for user to turn it on
         if (viewModel.isBluetoothEnabled()) {
@@ -44,16 +74,5 @@ class MainActivity : AppCompatActivity() {
             val enableBTIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBTIntent, viewModel.getRequestCode())
         }
-
-        viewModel.isBtConnected.observe(this, Observer {
-            binding.mainBluetoothIcon.setImageResource(R.drawable.ic_bluetooth_connected_24)
-            binding.mainBluetoothStatusImage.imageTintList = ColorStateList.valueOf(
-                ContextCompat.getColor(applicationContext, R.color.connected_green)
-            )
-            binding.mainTitle.text = getString(R.string.main_title_connected)
-            binding.mainBluetoothStatusText.text = getString(R.string.main_bt_status_connected)
-            binding.mainControlButton.visibility = View.VISIBLE;
-            binding.mainControlButton.isEnabled = true;
-        })
     }
 }
